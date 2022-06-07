cf3f57849f773e0b31cbf75c0dce30457f8244f1
Jean Moura <jeanmoura@outlook.com>
2022-06-07 19:25:05 -0300

    Saving some resusable code.

---
+++ arraytest.js
@@ -0,0 +1,27 @@
+// Can store several types of data:
+
+let array = ['string', [array1], [array2], [array3], [array4]];
+//console.log(array[3]);
+//For each
+//array.forEach(function(item, index){console.log(item, index)});
+
+//array.push('novo-item');
+//console.log(array);
+
+//array.pop();
+//console.log(array);
+
+//array.shift(); 
+//console.log(array);
+
+//array.unshift('novo item no inicio');
+//console.log(array);
+
+//array.indexOf();
+//console.log(array);
+
+//array.splice();
+//console.log(array);
+
+//array.slice();
+//console.log.(array);
