4e3fb4941c30a01a0ee84c941fca0294cc4d33ec
Jean Moura <jeanmoura@outlook.com>
2022-06-07 22:27:02 -0300

    Added ternary eg.

--- conditions/cond.js
+++ conditions/cond.js
@@ -2,6 +2,8 @@
 var player1 = 1;
 var player2 = 0;
 
+
+player1 != -1 && player2 != -1 ? console.log('Players are valid') : console.log('Players are invalid');
 if (player1 > 0) {
     console.log('player 1 scored');
 } else if (player2 > 0) {
