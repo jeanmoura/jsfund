1ddd9ed4bfc9d5886aaaa725851db701a6897b56
Jean Moura <jeanmoura@outlook.com>
2022-06-07 22:34:02 -0300

    Added a switch case.

--- conditions/cond.js
+++ conditions/cond.js
@@ -1,6 +1,7 @@
 
 var player1 = 1;
 var player2 = 0;
+var score;
 
 
 player1 != -1 && player2 != -1 ? console.log('Players are valid') : console.log('Players are invalid');
@@ -10,4 +11,17 @@ if (player1 > 0) {
     console.log('player 2 scored');
 } else {
     console.log('Nobody scored');
+}
+
+switch (score) {
+    case score = player1 > player2:
+        console.log('Player 1 won');
+        break;
+    case score = player1 < player2:
+        console.log('Player 2 won');
+        break;
+
+    default:
+        console.log('Nobody won');
+        break;
 }
