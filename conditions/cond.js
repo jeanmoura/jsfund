aab7b36cecf6bddfff94730b15b36480838a6728
Jean Moura <jeanmoura@outlook.com>
2022-06-07 22:23:00 -0300

    Changed var inital value.

--- conditions/cond.js
+++ conditions/cond.js
@@ -1,7 +1,11 @@
 
-var player1 = 0;
+var player1 = 1;
 var player2 = 0;
 
 if (player1 > 0) {
     console.log('player 1 scored');
-} 
+} else if (player2 > 0) {
+    console.log('player 2 scored');
+} else {
+    console.log('Nobody scored');
+}
